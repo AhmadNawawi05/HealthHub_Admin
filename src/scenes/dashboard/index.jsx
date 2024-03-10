8135a2178d20e066faeea49193c2a72c5b003173
AhmadNawawi05 <21221753095@smkassalaambandung.sch.id>
2024-03-11 01:57:20 +0700

    first commit

---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header.jsx";
+import { Box } from "@mui/material";
+
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title={"Dashboard"} subtitle={"dahsboard page"} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
